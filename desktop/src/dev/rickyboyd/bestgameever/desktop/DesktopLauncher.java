205cc134cffcbfada2d5092912edb0232646b80e
= <=>
2019-10-12 16:20:33 +0100

    first commit

---
+++ desktop/src/dev/rickyboyd/bestgameever/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package dev.rickyboyd.bestgameever.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import dev.rickyboyd.bestgameever.BestGameEver;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BestGameEver(), config);
+	}
+}
